7791dfed341bc01a03c337521cfac3b946fd8556
learnuidev <learnuidev@gmail.com>
2025-02-09 02:44:18 -0500

    fix  build failure

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverActions: {
-    // edit: updated to new key. Was previously `allowedForwardedHosts`
-    allowedOrigins: ["http://localhost:3001", "http://localhost:3000"],
-  },
 };
 
 export default nextConfig;
